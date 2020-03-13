dc7ef8d36143a0978454362a4200f5932a78e64b
Carlos Ariel <61167881+CarlosAriel98@users.noreply.github.com>
2020-03-13 13:57:33 -0600

    Se agregaron todas las funciones faltantes

--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
 # -*- coding: utf8 -*-
 import sys
+import os
 import platform
 from servicio_mensaje import ShortMessageService
 
@@ -12,11 +13,12 @@ class Menu:
     def __init__(self):
         self.service_message = ShortMessageService()
         self.options = { "1": self.add_new_arrival,
-                         #"2": self.message_count,
-                         #"3": self.get_message,
-                         #"4": self.delete_menssage,
-                         #"5": self.clear,
-                         "6": self.exit
+                         "2": self.message_count,
+                         "3": self.get_unread_index,
+                         "4": self.get_message,
+                         "5": self.delete_menssage,
+                         "6": self.clear,
+                         "7": self.exit
                         }
         
 
@@ -33,6 +35,25 @@ class Menu:
                7. Salir
               """)
 
+    
+    def press_enter(self):
+        """Realisa una pausar pare pedirle al usuario precionar una tecla"""
+        input("\nPrecione Enter para continuar")
+
+
+    def clear_screen(self):
+        """
+        verificar la plataforma del sistema operatico
+        del usuario y limpia la pantalla dependiendo del SO del usuario
+        """
+        if platform.system() == "Windows":
+            os.system("cls")
+        elif platform.system() == "Darwin" or platform.system() == "Linux":
+            os.system("clear")
+        else:
+            print("Plataforma no soportada")
+
+            
 
     def run(self):
         """ Metodo de entrar para la aplicacion"""
@@ -48,16 +69,60 @@ class Menu:
 
 
     def add_new_arrival(self):
+        self.clear_screen()
         from_number = input("Ingrese el numero: ")
         text_of_sms = input("Agreger el mensaje: ")
-        has_been_view="Noleido"
-        #self.service_message.add_new_arrival(from_number, text_of_sms, has_been_view)
+        has_been_view="noleido"
+        self.service_message.add_new_arrival(from_number, text_of_sms, has_been_view)
         print("Mensaje agregado")
+        self.press_enter()
+
+
+    def message_count(self):
+        self.service_message.message_count()
+        self.press_enter()
+
+    
+    def get_unread_index(self, leido="noleido"):
+        self.clear_screen
+        """ Busca los mensajes no leidos """
+        My_box = self.service_message.my_box
+        for box in My_box:
+            print("ide: {0}\nNumero: {1}\ntexto: {2}\nFecha: {3} {4}"
+             .format(box.id, box.from_nunver, box.text_of_sms, box.create_time, box.has_been_viewed))
+        self.press_enter()
+
+
+    def get_message(self):
+        """ Despliega una nota"""
+        self.clear_screen()
+        leido="ledio"
+        idmessage = input("Ingrese el ide del mensaje: ")
+        My_box = self.service_message.my_box
+        for box in My_box:
+            if str(box.id) == str(idmessage):
+                print("Id: {0}\nNumero: '{1}'\ntexto: {2}\nFecha: {3} {4}"
+                .format(box.id, box.from_number, box.text_of_sms, box.create_time, box.has_been_viewed))
+                self.service_message.modify_message(box.id, box.from_number, box.text_of_sms, leido)
+        self.press_enter()
+
+
+    def delete_menssage(self):
+        idmessage = input("ingrese el id del mensaje: ")
+        self.service_message.delete_message(idmessage)
+        self.press_enter()
+
+    
+    def clear(self):
+        self.service_message.clear()
+        self.press_enter()
 
     
     def exit(self):
+        """Cierra la aplicacion"""
         sys.exit()
 
 if __name__ == "__main__":
     menu = Menu()
     menu.run()
+
