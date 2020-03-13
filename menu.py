63668ffe3b6f65253e869b397e51e969a4d32964
CarlosAriel98 <c.amarquez14@hotmail.com>
2020-03-12 19:38:52 -0600

    Cambios pequeños y se agrega el inicio del menu

---
+++ menu.py
@@ -0,0 +1,13 @@
+# -*- coding: utf8 -*-
+import sys
+import platform
+from servicio_mensaje import ShortMessageService
+
+
+class Menu:
+    """
+    Crea el menu principal para el usuario
+    """
+
+    def __init__(parameter_list):
+        pass
--- servicio_mensaje.py
+++ servicio_mensaje.py
@@ -18,8 +18,37 @@ class ShortMessageService:
         en_Lista = str(len(self.my_box))
     
     
-    def get_unread_indexes(self):
+    def get_unread_indexes(self, has_been_viewed = "Noleido"):
         """ Muestra la lista de los mensajes sin leer """
-        for msm in self:
-            if str(m.id) == str(note_id):
-                return NotADirectoryError
+        for msm in self.my_box:
+            if str(msm.has_been_viewed) == str(has_been_viewed):
+                return msm
+
+    
+    def _search_note(self, id_message):
+        """
+        Busca los mensajes enviados
+        Funcion privada
+        """
+
+        for sms in self.my_box:
+            if str(msm.id) == str(id_message):
+                return msm
+        
+        return None
+
+    def delete_message(self, id_message)
+    """ Elimina un mensaje"""
+    msm = self._search_note(id_message)
+
+    if sms:
+        sefl.my_box.remove(id_message)
+        return True
+    else:
+        print("no existe un mensaje con el id: {0}")
+        self.format(id_message)
+        return False
+    
+    def clear(self):
+        """ Elimina todos los mensajes"""
+    
